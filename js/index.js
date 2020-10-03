b8a37b72f1cfd3aec9e31abe5f562d1d701211af
Hazel Meades <hazel@meades.org>
2020-10-03 15:33:54 +0100

    Tab Functionality
    Added the tab menu and put the pie chart and key in the gender section.

--- js/index.js
+++ js/index.js
@@ -84,8 +84,8 @@ var data=[{
 
  Plotly.newPlot('myDiv', data, layout, {scrollZoom: true});
  
- /* Tab function */
- function openCity(evt, cityName) {
+ /* Tab function 
+ function openCategory(evt, diversityCategory) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -95,7 +95,7 @@ var data=[{
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(diversityCategory).style.display = "block";
   evt.currentTarget.className += " active";
-}
+}*/
     
